6f5b16b050fab155227615096f858793bf9f615c
shadman-a <shadman@outlook.com>
2020-12-18 15:24:05 -0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int iarr [][] = new int [2][2];
+
+        iarr[0][0] = 10;
+        iarr[0][1] = 10;
+        iarr[1][0] = 10;
+        iarr[1][1] = 10;
+
+        System.out.println(iarr[0][0]);
+
+    }
+}
